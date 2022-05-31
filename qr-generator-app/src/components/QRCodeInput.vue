1af2f1284102524865f86eea2622a5e00ebb5331
camilaleniss <mlmariacami2@gmail.com>
2022-05-30 23:33:22 -0500

    Add test samples to know if recognize ant design components

--- qr-generator-app/src/components/QRCodeInput.vue
+++ qr-generator-app/src/components/QRCodeInput.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello" align-center justify-center>
-    <a-input v-model:value="qrCodeInput"></a-input>
-    <a-button v-on:click="sendQRCode()">Generar QR</a-button>
+    <a-input id="txt-qr-code" v-model:value="qrCodeInput"></a-input>
+    <a-button id="btn-generate" v-on:click="sendQRCode()">Generar QR</a-button>
   </div>
 </template>
 
@@ -10,7 +10,7 @@ export default {
   name: 'QrCodeInput',
   data: function () {
     return {
-      qrCodeInput: ''
+      qrCodeInput: 'Camila'
     }
   },
   methods: {
@@ -20,20 +20,3 @@ export default {
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus">
-h3
-  margin 40px 0 0
-
-ul
-  list-style-type none
-  padding 0
-
-li
-  display inline-block
-  margin 0 10px
-
-a
-  color #42b983
-</style>
--- qr-generator-app/tests/unit/example.spec.js
+++ qr-generator-app/tests/unit/example.spec.js
@@ -1,12 +1,43 @@
-import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import { shallowMount, mount } from '@vue/test-utils'
+import QRCodeInput from '@/components/QRCodeInput.vue'
+import Antd from 'ant-design-vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('QRCodeInput.vue', () => {
+  it('renders qr code input component', () => {
+    const wrapper = shallowMount(QRCodeInput)
+
+    const component = wrapper.find('.hello')
+
+    expect(component.classes()).toContain('hello')
+  })
+  it('renders qr code input component with its depedencies', () => {
+    const wrapper = mount(QRCodeInput,
+      {
+        global:
+        { plugins: [Antd] }
+      })
+
+    const component = wrapper.find('#btn-generate')
+
+    expect(component.text()).toBe('Generar QR')
+  })
+  it('change qr code input and renders it', () => {
+    const wrapper = mount(QRCodeInput,
+      {
+        global:
+        { plugins: [Antd] }
+      })
+
+    let component = wrapper.find('#txt-qr-code')
+
+    console.log(wrapper.html())
+
+    expect(component.text()).toBe('')
+
+    wrapper.vm.qrCodeInput = 'Dummy input'
+
+    component = wrapper.find('.el-input')
+
+    expect(component.text()).toBe('Dummy input')
   })
 })
