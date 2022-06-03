6d58858050bab3e07c001e36b083d6bb7b886e0d
camilaleniss <mlmariacami2@gmail.com>
2022-06-02 22:00:19 -0500

    Refactor mounting a component tests to be more readable

--- qr-generator-vue-3/tests/unit/components/qr_code_input.spec.js
+++ qr-generator-vue-3/tests/unit/components/qr_code_input.spec.js
@@ -3,12 +3,6 @@ import { createStore } from 'vuex'
 import QRCodeInput from '@/components/QRCodeInput.vue'
 
 describe('QRCodeInput.vue', () => {
-  const store = createStore({
-    state () {
-      return { count: 1 }
-    }
-  })
-
   describe('mounting a component', () => {
     it('renders qr code input component', () => {
       const wrapper = shallowMount(QRCodeInput)
@@ -18,26 +12,31 @@ describe('QRCodeInput.vue', () => {
       expect(component.classes()).toContain('hello')
     })
     it('renders qr code input component with its depedencies', () => {
+      const store = createStore({
+        state () {
+          return { count: 1 }
+        }
+      })
+
       const wrapper = shallowMount(QRCodeInput,
         {
           global:
-          { plugins: [store] }
+            { plugins: [store] }
         })
+      it('renders generate qr code button with the text ', () => {
+        const component = wrapper.find('#btn-generate')
 
-      const component = wrapper.find('#btn-generate')
+        expect(component.text()).toBe('Generar QR')
+      })
+      it('renders txt input, change its value and see if stored', () => {
+        const component = wrapper.find('#txt-qr-code')
 
-      expect(component.text()).toBe('Generar QR')
-    })
-    it('change qr code input and renders it', () => {
-      const wrapper = shallowMount(QRCodeInput)
+        expect(component.element.value).toBe('')
 
-      const component = wrapper.find('#txt-qr-code')
+        component.setValue('www.platzi.com')
 
-      expect(component.element.value).toBe('')
-
-      component.setValue('www.platzi.com')
-
-      expect(wrapper.vm.qrCodeInput).toBe('www.platzi.com')
+        expect(wrapper.vm.qrCodeInput).toBe('www.platzi.com')
+      })
     })
   })
   describe('actions and mocks', () => {
